acb62eb9460f150f1963bd870ff65e0113d20243
dhsong215 <77286910+dhsong215@users.noreply.github.com>
2023-01-10 11:56:04 +0900

    small update

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Detail from "./routes/Detail";
 
 const App = () => {
   return (
-    <Router>
+    <Router basename={process.env.PUBLIC_URL}>
       <Routes>
         <Route path="/movie/:id" element={<Detail />} />
         <Route path="/" element={<Home />} />
